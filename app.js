59aad4604f8f617fa2fc9517187678697cd82e3e
Riley Todd <rileyatodd@gmail.com>
2016-02-28 10:24:56 -0600

    Initial commit

---
+++ app.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var vhost = require('vhost');
+
+var habRabApp = require('../habitRabbitv2/app.js');
+var dumpsterApp = require('../dumpster/app.js');
+var personalSite = require('../personalSite/app.js');
+var mygame = require('../mygame/app.js');
+var ipg = require('../indyPulltabGaming/app.js');
+
+var vhoster = express();
+
+vhoster.use(vhost('habitrabbit.com', habRabApp));
+vhoster.use(vhost('the-dumpster.com', dumpsterApp));
+vhoster.use(vhost('rileyatodd.com', personalSite));
+vhoster.use(vhost('minimagic.com', mygame));
+vhoster.use(vhost('indianapulltabs.com', ipg));
+
+// catch 404 and forward to error handler
+vhoster.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+module.exports = vhoster;
