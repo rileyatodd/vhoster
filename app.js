e4e3381def51e6e9277f3179266597298430b6e2
Riley Todd <rileyatodd@gmail.com>
2016-03-13 10:49:22 -0500

    Added support for dev subdomains

--- app.js
+++ app.js
@@ -22,6 +22,12 @@ vhoster.use(vhost('www.minimagic.com', mygame));
 vhoster.use(vhost('indianapulltabs.com', ipg));
 vhoster.use(vhost('www.indianapulltabs.com', ipg));
 
+if (vhoster.get('env') === 'dev') {
+  vhoster.use(vhost('dev.the-dumpster.com', dumpsterApp));
+  vhoster.use(vhost('dev.rileyatodd.com', personalSite));
+  vhoster.use(vhost('dev.minimagic.com', mygame));
+  vhoster.use(vhost('dev.indianapulltabs.com', ipg));
+}
 
 // catch 404 and forward to error handler
 vhoster.use(function(req, res, next) {
