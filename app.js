326b43af813a44be0f04b0f48e50922839dbe8e7
Riley Todd <rileyatodd@gmail.com>
2016-08-01 15:17:22 -0500

    Updated vhoster to read config from file

--- app.js
+++ app.js
@@ -1,39 +1,20 @@
-var express = require('express');
-var path = require('path');
-var logger = require('morgan');
-var vhost = require('vhost');
+var express = require('express')
+var vhost = require('vhost')
 
-var dumpsterApp = require('../dumpster/app.js');
-var personalSite = require('../personalSite/app.js');
-var mygame = require('../my-game/app.js');
-var ipg = require('../indyPulltabGaming/app.js');
+var vhoster = express()
 
-var vhoster = express();
+var config = require(process.env.CONFIG_PATH || './vhoster.config.json')
 
-vhoster.use(vhost('the-dumpster.com', dumpsterApp));
-vhoster.use(vhost('www.the-dumpster.com', dumpsterApp));
-
-vhoster.use(vhost('rileyatodd.com', personalSite));
-vhoster.use(vhost('www.rileyatodd.com', personalSite));
-
-vhoster.use(vhost('minimagic.com', mygame));
-vhoster.use(vhost('www.minimagic.com', mygame));
-
-vhoster.use(vhost('indianapulltabs.com', ipg));
-vhoster.use(vhost('www.indianapulltabs.com', ipg));
-
-if (vhoster.get('env') === 'dev') {
-  vhoster.use(vhost('dev.the-dumpster.com', dumpsterApp));
-  vhoster.use(vhost('dev.rileyatodd.com', personalSite));
-  vhoster.use(vhost('dev.minimagic.com', mygame));
-  vhoster.use(vhost('dev.indianapulltabs.com', ipg));
-}
+Object.keys(config.vhosts).forEach(function(url) {
+  var appPath = config.vhosts[url]
+  vhoster.use(vhost(url, require(appPath)))
+})
 
 // catch 404 and forward to error handler
 vhoster.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+  var err = new Error('Not Found')
+  err.status = 404
+  next(err)
+})
 
-module.exports = vhoster;
+module.exports = vhoster
